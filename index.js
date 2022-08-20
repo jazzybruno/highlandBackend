8460f27d939f6d473d43c4d1bc370f4e3eacbfac
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2022-08-20 19:47:52 +0200

    Fixing the port Issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { connection } = require('./utils/mongodb');
-const PORT = process.env | 5000;
+const PORT = process.env.PORT || 3000
 const app = express();
 const Middlewares = require('./utils/middlewares');
 require('dotenv').config()

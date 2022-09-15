2f1ae8c5e1ca1f94f31a42298fc4bed160e86741
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2022-09-15 17:10:02 +0200

    Removed Auth for Admission

--- routes/admission.routes.js
+++ routes/admission.routes.js
@@ -5,7 +5,7 @@ const student = require('../middlewares/admission');
 const auth = require('../middlewares/auth');
 
 
-AdmissionRouter.post('/' , auth , student.single('image') , newAdmission);
+AdmissionRouter.post('/' , student.single('image') , newAdmission);
 AdmissionRouter.get('/' , auth , getAdmissions);
 AdmissionRouter.get('/:id' , auth , getAdmissionById);
 

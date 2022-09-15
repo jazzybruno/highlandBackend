c3728e53f8be0633cff71d4a24e4e8b1ac6bdd3c
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2022-09-15 17:30:47 +0200

    Fixing issues with enrollemnt

--- middlewares/admission.js
+++ middlewares/admission.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './images/admission');
+        cb(null, './images');
     },
 
     filename: (req, file, cb) => {

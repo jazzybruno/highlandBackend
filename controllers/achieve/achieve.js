82120174c59651aa5512246a93e29ed59d84a6f8
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2022-09-19 19:08:48 +0200

    Adde more routes in the project

--- controllers/achieve/achieve.js
+++ controllers/achieve/achieve.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { Achievement } = require('../../models/achievement')
+const Joi = require()
 
 const getAchievements = async(req , res) => {
      try {
@@ -46,10 +47,116 @@ const getAchievementById = async(req , res) => {
     }
 }
 
-const postAchievement = async ()=> {
+const postAchievement = async (req , res)=> {
 
+    const data = {
+        title : req.body.title,
+        desc: req.body.desc
+    }
+    const {error} = validateAchievement(data)
+
+    if(error){
+        return res.status(400).json({
+            message: error.details[0].message
+        })
+    }
+
+    const achievement = new Achievement(data)
+
+    try {
+
+        await achievement.save()
+        res.status(200).json({
+            message: 'Achievement saved successfully'
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error while creating contact',
+            error: error.message
+        })
+    }
  
-
 
 }
 
+const updateAchievement = async (req , res)=> {
+
+    const id = req.params.id
+
+    try {
+
+        const achievement = await Achievement.findById(id)
+
+        if(!achievement){
+            return res.status(200).json({
+                message: "No Achievmenst with this id was found"
+            })
+        }
+
+        const data = {
+            title : req.body.title,
+            desc: req.body.desc
+        }
+        const {error} = validateAchievement(data)
+    
+        if(error){
+            return res.status(400).json({
+                message: error.details[0].message
+            })
+        }
+
+        achievement.title = req.body.title
+        achievement.desc = req.body.desc
+    
+    
+        try {
+    
+            await achievement.save()
+            res.status(200).json({
+                message: 'Achievement updated successfully'
+            });
+            
+        } catch (error) {
+            res.status(500).json({
+                message: 'Error while creating contact',
+                error: error.message
+            })
+        }
+
+        
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server error"
+        })
+    }
+
+}
+
+const deleteAchievement = async (req , res)=> {
+    const id = req.params.id
+
+    try {
+
+        const achievement = await Achievement.findByIdAndDelete(id)
+        res.status(200).json({
+            message: 'Achievement deleted successfully'
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error while creating contact',
+            error: error.message
+        })
+    }
+}
+
+
+
+function validateAchievement (achievement) {
+    const schema = Joi.object({
+        title: Joi.string().min(10).required(),
+        desc: Joi.string().min(15).required(),
+    })
+    return schema.validate(achievement)
+}
